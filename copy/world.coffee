c7bb848f0f7a8389011e4e4a964f89ca3f210cca
Paul Jensen <paulbjensen@gmail.com>
2013-01-26 22:45:32 +0000

    bumped version to 0.0.6 - selenium is now booted only once

--- copy/world.coffee
+++ copy/world.coffee
@@ -2,17 +2,23 @@ process.env["SS_ENV"] = "cucumber"
 selenium          = require '../../node_modules/ss-cucumber/node_modules/selenium-launcher'
 soda              = require '../../node_modules/ss-cucumber/node_modules/soda'
 app               = require '../../app'
+browser           = null
 
 World = (callback) ->
-  
-  selenium (err,selenium) ->
-    @browser = soda.createClient
-      host:     selenium.host
-      port:     selenium.port
-      url:      "http://localhost:3000"
-      browser:  "firefox"
 
+  if browser is null  
+    selenium (err,selenium) ->
+      browser = soda.createClient
+        host:     selenium.host
+        port:     selenium.port
+        url:      "http://localhost:3000"
+        browser:  "firefox"
+
+      @browser = browser
+      callback {@browser}
+      process.on 'exit', -> selenium.kill()
+  else
+    @browser = browser
     callback {@browser}
-    process.on 'exit', -> selenium.kill()
 
 exports.World = World
--- copy/world.js
+++ copy/world.js
@@ -2,21 +2,31 @@ process.env["SS_ENV"] = "cucumber";
 var selenium          = require('../../node_modules/ss-cucumber/node_modules/selenium-launcher');
 var soda              = require('../../node_modules/ss-cucumber/node_modules/soda');
 var app               = require('../../app');
+var browser           = null;
 
 var World = function(callback){
-  
-  selenium(function(err,selenium){
-    this.browser = soda.createClient({
-      host:     selenium.host,
-      port:     selenium.port,
-      url:      "http://localhost:3000",
-      browser:  "firefox"
+
+  if (browser == null) {
+    selenium(function(err,selenium){
+      browser = soda.createClient({
+        host:     selenium.host,
+        port:     selenium.port,
+        url:      "http://localhost:3000",
+        browser:  "firefox"
+      });
+
+      this.browser = browser;
+      callback({browser: this.browser});
+      process.on('exit', function(){
+        selenium.kill();
+      });
     });
+
+  } else {
+    this.browser = browser;
     callback({browser: this.browser});
-    process.on('exit', function(){
-      selenium.kill();
-    });
-  })
+  }
+  
 };
 
 exports.World = World;
